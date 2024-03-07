90400036d7d52ffcd2bf293b37a501ed40b3a1b3
Kruk Roman <roma_0428_3675@ukr.net>
2024-03-07 14:56:11 +0200

    Добавьте файлы проекта.

---
+++ 5.1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int rollDice() {
+    return rand() % 6 + 1;
+}
+
+int main() {
+    srand(time(0));
+
+    int userScore = 0;
+    int computerScore = 0;
+
+    for (int i = 0; i < 3; ++i) {
+        cout << "Player rolls the dice..." << endl;
+        int userRoll1 = rollDice();
+        int userRoll2 = rollDice();
+        int userTotal = userRoll1 + userRoll2;
+        cout << "Player rolled: " << userRoll1 << " and " << userRoll2 << " (total: " << userTotal << ")" << endl;
+        userScore += userTotal;
+
+        cout << "Computer rolls the dice..." << endl;
+        int compRoll1 = rollDice();
+        int compRoll2 = rollDice();
+        int compTotal = compRoll1 + compRoll2;
+        cout << "Computer rolled: " << compRoll1 << " and " << compRoll2 << " (total: " << compTotal << ")" << endl;
+        computerScore += compTotal;
+    }
+
+    cout << "Player's score: " << userScore << endl;
+    cout << "Computer's score: " << computerScore << endl;
+    if (userScore > computerScore) {
+        cout << "Player wins!" << endl;
+    }
+    else if (computerScore > userScore) {
+        cout << "Computer wins!" << endl;
+    }
+    else {
+        cout << "Draw" << endl;
+    }
+
+}
+
+
